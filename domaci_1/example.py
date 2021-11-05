1eec4123edaa94e324f3393f0d9c8b01fe151dd3
Viktor <todosijevicviktor998@gmail.com>
2021-11-05 11:48:15 +0100

    Plotovao primere za 1d i 2d, skaliranje zavisi od dimenzije

--- domaci_1/example.py
+++ domaci_1/example.py
@@ -2,16 +2,26 @@ import numpy as np
 from matplotlib import pyplot as plt
 from locally_weighted_lr import get_W
 from data_loading import add_bias
+from mpl_toolkits.mplot3d import Axes3D
 
 
-def get_example(size):
-    # return np.random.uniform(low=-2 * np.pi, high=2 * np.pi, size=size)
-    return np.linspace(start=-2 * np.pi, stop=2 * np.pi, num=size)
+def lwlr(x_samples, x_train, y_train, tau):
+    y_samples = np.zeros(x_samples.shape[0])
+    for i in range(x_samples.shape[0]):
+        W = get_W(x_samples[i, np.newaxis], x_train, tau=tau)
+
+        X = add_bias(x_train)
+
+        theta = ((X.T @ W @ X) ** (-1)) @ X.T @ W @ y_train
+        X_sample = add_bias(x_samples[i, np.newaxis])
+        y_samples[i] = X_sample @ theta
+
+    return y_samples
 
 
-if __name__ == '__main__':
-    x_train = get_example(size=500)
-    y_train = np.exp(x_train) + 0.1 * np.random.randn(len(x_train))
+def example_1d():
+    x_train = np.linspace(start=-2 * np.pi, stop=2 * np.pi, num=500)
+    y_train = np.sin(x_train) + 0.1 * np.random.randn(len(x_train))
 
     x_train = x_train.reshape((len(x_train), 1))
 
@@ -24,19 +34,41 @@ if __name__ == '__main__':
 
     x_samples = np.random.uniform(low=-2 * np.pi, high=2 * np.pi, size=100)
     x_samples = x_samples.reshape((len(x_samples), 1))
-    y_samples = np.zeros_like(x_samples)
-    for i in range(x_samples.shape[0]):
-        W = get_W(x_samples[i].reshape(x_samples[i].shape[0], 1), x_train, tau=0.1)
 
-        X = add_bias(x_train)
-
-        # Mora da se deli sa 4, ne znam zasto
-        theta = ((X.T @ W @ X) ** (-1)) @ X.T @ W @ y_train / 4
-        X_sample = add_bias(x_samples[i].reshape(x_samples[i].shape[0], 1))
-        y_samples[i] = X_sample @ theta
-
+    y_samples = lwlr(x_samples, x_train, y_train, tau=0.1)
     plt.plot(x_samples, y_samples, 'rx')
 
     plt.show()
 
-    # TODO Probati za 2D
+
+def example_2d():
+    nx, ny = (40, 40)
+    x = np.linspace(-2 * np.pi, 2 * np.pi, nx)
+    y = np.linspace(-2 * np.pi, 2 * np.pi, ny)
+    X, Y = np.meshgrid(x, y)
+
+    z = np.cos(X) + np.cos(Y) + 0.1 * np.random.randn(nx, ny)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.scatter(X, Y, z)
+    # ax.plot_surface(xv, yv, np.cos(xv) + np.cos(yv), color='r')
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    x_samples = np.random.uniform(low=-2 * np.pi, high=2 * np.pi, size=(500, 2))
+    x_train = np.hstack((X.flatten().reshape(nx * ny, 1), Y.flatten().reshape(nx * ny, 1)))
+    y_train = z.flatten()
+
+    y_samples = lwlr(x_samples, x_train, y_train, tau=0.1)
+
+    ax.scatter(x_samples[:, 0], x_samples[:, 1], y_samples, color='r')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    example_1d()
+    example_2d()
